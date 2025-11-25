d5bed71cea4deaa0dc844955e9dd05c6440b0f6a
Paul Robinette <paulrobinette@gmail.com>
2025-11-25 09:08:41 -0500

    typo in path

--- scripts/setup_install.sh
+++ scripts/setup_install.sh
@@ -1,5 +1,5 @@
 udisksctl mount -b /dev/nvme0n1p1
 udisksctl mount -b /dev/nvme0n1p2
-sudo mkdir /media/student/writable/home/shared
+sudo mkdir -p /media/student/writable/home/shared
 sudo cp /home/student/alexbot-code/scripts/install.sh /media/student/writable/home/shared/install.sh
 sudo chmod 777 /media/student/writable/home/shared/install.sh
