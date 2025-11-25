1a84eea87ba0dc3c04c45567290a71068efd8819
Paul Robinette <paulrobinette@gmail.com>
2025-11-25 09:09:19 -0500

    removed unneeded mount

--- scripts/setup_install.sh
+++ scripts/setup_install.sh
@@ -1,4 +1,3 @@
-udisksctl mount -b /dev/nvme0n1p1
 udisksctl mount -b /dev/nvme0n1p2
 sudo mkdir -p /media/student/writable/home/shared
 sudo cp /home/student/alexbot-code/scripts/install.sh /media/student/writable/home/shared/install.sh
