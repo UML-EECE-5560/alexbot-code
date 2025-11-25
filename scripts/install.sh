88c274cc11878c68858fa86fcd6783d1024a4e7e
Paul Robinette <paulrobinette@gmail.com>
2025-11-24 21:50:45 -0500

    added libcamera build to script

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ sudo apt -y install vim emacs git openssh-server
 
 # ROS install
 
-sudo apt install software-properties-common
+sudo apt -y install software-properties-common
 sudo add-apt-repository universe
 
 sudo apt update && sudo apt install curl -y
@@ -21,6 +21,21 @@ export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrast
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
-sudo apt update && sudo apt install ros-dev-tools
-sudo apt install ros-jazzy-desktop
+sudo apt update && sudo apt -y install ros-dev-tools
+sudo apt -y install ros-jazzy-desktop
+
+# Build libcamera
+
+sudo apt install -y python-pip git python3-jinja2
+sudo apt install -y libboost-dev
+sudo apt install -y libgnutls28-dev openssl libtiff-dev pybind11-dev
+sudo apt install -y qtbase5-dev libqt5core5a libqt5widgets
+sudo apt install -y meson cmake
+sudo apt install -y python3-yaml python3-ply
+sudo apt install -y libglib2.0-dev libgstreamer-plugins-base1.0-dev
+
+git clone https://github.com/raspberrypi/libcamera.git
+cd libcamera
+meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=true -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
+ninja -C build install
 
