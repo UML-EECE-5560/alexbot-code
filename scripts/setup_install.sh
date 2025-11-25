035dbc271ee1bcd1c69cb91309b8362877c89307
Paul Robinette <paulrobinette@gmail.com>
2025-11-25 09:05:37 -0500

    edited mount

--- scripts/setup_install.sh
+++ scripts/setup_install.sh
@@ -1,5 +1,5 @@
-udisksctl mount -b /dev/nvme0n1p1
-udisksctl mount -b /dev/nvme0n1p2
+sudo udisksctl mount -b /dev/nvme0n1p1
+sudo udisksctl mount -b /dev/nvme0n1p2
 sudo mkdir /media/student/writable/home/shared
 sudo cp /home/student/alexbot-code/scrips/install.sh /media/student/writable/home/shared/install.sh
 sudo chmod 777 /media/student/writable/home/shared/install.sh
